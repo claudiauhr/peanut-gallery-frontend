b2f7ff3bbeeebef49aec68c287f0d232422f008d
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-01-17 09:45:54 -0500

    refactoring routes to other pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
-
-import './assets/index.css'
+import './assets/index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const ROOT = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,7 +11,9 @@ const build = () => {
 
     ROOT.render(
         <React.StrictMode>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </React.StrictMode>
     );
 }
