22d7af15bb19a8c3aca8ad39e44096baffa31559
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-01-17 12:22:20 -0500

    adds login page and styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,25 @@
+import Header from "../components/Header";
+
 function Login() {
     return (
-        <form>
-            <label htmlFor="email">email</label>
-            <input type="email" placeholder="email@email.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
-            <input type="password" placeholder="********" id="password" name="password" />
-            <button>Log In</button>
+        <>
+        <Header />
+        <div class="login">
+        <h1>Login</h1>
+        <form action="/login" method="GET">
+            <fieldset class="login-info">
+            <legend>Email & Password</legend>
+            <label for = "email">E-MAIL</label>
+            <input type="email" id="email"/>
+            <label for ='password'>PASSWORD</label>
+            <input type="password" id="password"/>
+            </fieldset>
+            <input type="submit" value="Login"/>
         </form>
-    )
-}
+        </div>
+        </>
+    );
+};
 
 export default Login;
 
