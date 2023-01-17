a11a3532ec598e1b661542b90a56c749f97cf4bc
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-01-17 12:07:08 -0500

    adds registration page, nav, styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
+        <div className="nav">
             <div><Link to="/">Home</Link></div>
             <div><Link to="/games">Game</Link></div>
             <div><Link to="/login">Log In</Link></div>
             <div><Link to="/register">Register</Link></div>
+            <div><Link to="/about">About and How-To's</Link></div>
         </div>
     )
 }
