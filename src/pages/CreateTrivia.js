ee3d82c4a954131a8a2d347fbcddefea57184d00
Scynes <dustin.greyfield@gmail.com>
2023-01-18 16:58:15 -0500

    refactor/wip: some css and began create trivia implementation

--- src/pages/CreateTrivia.js
+++ src/pages/CreateTrivia.js
@@ -1,10 +1,9 @@
 function CreateTrivia() {
     return (
-        <form>
-            <label htmlFor="name">Game Name</label>
-            <input type="name" placeholder="game name" />
-            <button>Add Game</button>
-        </form>
+        <div className="create-trivia-container">
+            <div>one</div>
+            <div>two</div>
+        </div>
     )
 }
 
