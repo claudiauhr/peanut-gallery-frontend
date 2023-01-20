97c1ce8833cd25d2f754c41f55287c38029ada50
audemusprime <edbrossa@gmail.com>
2023-01-20 13:44:50 -0500

    adds registration functionality

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,26 +1,59 @@
-import Header from "../components/Header";
+import Header from '../components/Header';
+import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
 
-function Register() {
-    return (
-        <>
-        <Header />
-        <div className="registration">
-          <h2>Registration</h2>
-          <form action="/register" method="POST">
-                <fieldset>
-                <legend>Player Info</legend>
-                <label htmlFor ="username">USERNAME</label>
-                <input type="text" name="username" placeholder="USERNAME"/>
-                <label htmlFor ="password">PASSWORD</label>
-                <input type="password" name="password" placeholder="PASSWORD"/>
-                <label htmlFor = "email">E-MAIL</label>
-                <input type="email" name="email" placeholder="EMAIL"/>
-                </fieldset>
-                <input type="submit" value="REGISTER"/>
-          </form>
-        </div>
-        </>
-    );
-};
+function Register(props) {
+	const navigate = useNavigate();
+	const acctForm = {
+		email: '',
+		username: '',
+		password: '',
+	};
+	const [
+		form,
+		setForm,
+	] = useState(acctForm);
+
+	const handleChange = (e) => {
+		setForm({...form, [e.target.name]: e.target.value});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newAcct = async (account) => {
+			const URL = 'http://localhost:4000/c/register';
+			await fetch(URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'Application/JSON',
+				},
+				body: JSON.stringify(account),
+			});
+		};
+		newAcct(form);
+		navigate('/game');
+	};
+
+	return (
+		<>
+			<Header />
+			<div className='registration'>
+				<h2>Registration</h2>
+				<form onSubmit={handleSubmit}>
+					<fieldset>
+						<legend>Player Info</legend>
+						<label htmlFor='email'>E-MAIL</label>
+						<input type='email' name='email' onChange={handleChange} placeholder='EMAIL' />
+						<label htmlFor='username'>USERNAME</label>
+						<input type='text' name='username' onChange={handleChange} placeholder='USERNAME' />
+						<label htmlFor='password'>PASSWORD</label>
+						<input type='password' name='password' onChange={handleChange} placeholder='PASSWORD' />
+					</fieldset>
+					<input type='submit' value='REGISTER' />
+				</form>
+			</div>
+		</>
+	);
+}
 
 export default Register;
