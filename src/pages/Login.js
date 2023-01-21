920b4357644eec705c75de46714be0229c1fb435
audemusprime <edbrossa@gmail.com>
2023-01-21 18:59:01 -0500

    WIP provides necessary imports

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,30 @@
-import Header from "../components/Header";
-import LoginDisplay from "../components/LoginDisplay";
-import JoinGame from "../components/JoinGame"
+import Header from '../components/Header';
+// import LoginDisplay from "../components/LoginDisplay";
+import JoinGame from '../components/JoinGame';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-    return (
-        <>
-        <Header />
-        <div className="login">
-        <h2>Login</h2>
-        <form action="/login" method="GET">
-        <fieldset>
-            <legend>Email & Password</legend>
-            <label htmlFor = "username">USERNAME</label>
-            <input type="username" name="email" placeholder="USERNAME"/>
-            <label htmlFor ='password'>PASSWORD</label>
-            <input type="password" name="password" placeholder="PASSWORD"/>
-        </fieldset>
-            <input type="submit" value="Login"/>
-        </form>
-        </div>
-        return (
-            isLoggedIn ? <LoginDisplay /> : <JoinGame />
-            )
-        </>
-    );
-};
+	return (
+		<>
+			<Header />
+			<div className='login'>
+				<h2>Login</h2>
+				<form action='/login' method='GET'>
+					<fieldset>
+						<legend>Email & Password</legend>
+						<label htmlFor='username'>USERNAME</label>
+						<input type='username' name='username' placeholder='USERNAME' />
+						<label htmlFor='password'>PASSWORD</label>
+						<input type='password' name='password' placeholder='PASSWORD' />
+					</fieldset>
+					<input type='submit' value='Login' />
+				</form>
+			</div>
+			{/* return ( isLoggedIn ? <LoginDisplay /> : <JoinGame />) */}
+		</>
+	);
+}
 
 export default Login;
 
